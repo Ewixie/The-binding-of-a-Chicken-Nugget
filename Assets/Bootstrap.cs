25e6af9d3be8bf81cdd3e510410c8b3435158343
Ewix <z061200123@gmail.com>
2024-05-24 21:09:04 +0300

    Movement added version 1.0 (not) 😁

--- Assets/Bootstrap.cs
+++ Assets/Bootstrap.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class Bootstrap : MonoBehaviour
 {
+    [Header("Player")]
+    [SerializeField] private PlayerController playerController;
+    
     private PlayerInput _playerInput;
+
     private void Awake()
     {
         _playerInput = new PlayerInput();
+        
+        playerController.Init(_playerInput);
     }
 
     private void Update()
